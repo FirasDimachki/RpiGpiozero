896955f6b507b45997d34ea99b4f8075c63e127f
Firas EL DIMACHKI <201911090@ua.edu.lb>
2024-05-15 23:48:18 +0300

    run 1

---
+++ rpi.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import time
+import gpiozero
+
+led = LED(27)
+button = Button(22)
+
+button.when_pressed = led.on
+button.when_released = led.off
+
+cl = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+cl.connect('127.0.0.1', 1883)
+cl.loop_start()
+
+time.sleep(10)
+
+cl.loop_stop()
+cl.disconnect()
+led.off()
+
+exit()
